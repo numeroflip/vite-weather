f0e9e759463a18666942fd47f07a85ca3e572806
Aron Berenyi <aron.berenyi@tuta.io>
2024-06-15 21:32:13 +0200

    add page skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
 function App() {
-  return <h1>App</h1>;
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    console.log(Array.from(formData.entries()));
+  };
+
+  return (
+    <>
+      <header>Header</header>
+      <h1>App</h1>
+      <main>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="latitude">Latitude</label>
+          <input type="text" id="latitude" name="latitude" />
+          <label htmlFor="longitude">Longitude</label>
+          <input type="text" id="longitude" name="longitude" />
+
+          <button type="submit">Submit</button>
+        </form>
+      </main>
+      <footer>Footer</footer>
+    </>
+  );
 }
 
 export default App;
